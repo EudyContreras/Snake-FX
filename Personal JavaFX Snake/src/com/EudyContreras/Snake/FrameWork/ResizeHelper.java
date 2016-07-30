abc8d4d7a11ecbab130cb4254276ebf9635e7fa5
Eudy Contreras <Darkyec777@gmail.com>
2016-07-31 00:45:34 +0200

    update

---
+++ Personal JavaFX Snake/src/com/EudyContreras/Snake/FrameWork/ResizeHelper.java
@@ -0,0 +1,216 @@
+package com.EudyContreras.Snake.FrameWork;
+
+import com.EudyContreras.Snake.FrameWork.CursorUtility.CursorID;
+import com.EudyContreras.Snake.ImageBanks.GameImageBank;
+
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.geometry.Bounds;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.stage.Stage;
+
+public class ResizeHelper {
+
+
+
+	public static void addSceneManipulation(Stage stage, Scene scene) {
+
+		GestureListener gestureListener = new GestureListener(stage,scene);
+
+		stage.getScene().addEventHandler(MouseEvent.MOUSE_MOVED, gestureListener);
+		stage.getScene().addEventHandler(MouseEvent.MOUSE_PRESSED, gestureListener);
+		stage.getScene().addEventHandler(MouseEvent.MOUSE_DRAGGED, gestureListener);
+		stage.getScene().addEventHandler(MouseEvent.MOUSE_EXITED, gestureListener);
+		stage.getScene().addEventHandler(MouseEvent.MOUSE_EXITED_TARGET, gestureListener);
+
+	}
+	private static class GestureListener implements EventHandler<MouseEvent> {
+
+		private final Scene scene;
+		private final Stage stage;
+		private double factor = 1.01;
+		private double border = 5;
+		private double baseWidth = 0;
+		private double baseHeight = 0;
+		private double yOffset = 0;
+		private double xOffset = 0;
+		private double stretchContextX = 0;
+		private double stretchContextY = 0;
+
+		private Cursor cursorEvent = Cursor.DEFAULT;
+
+		public GestureListener(Stage stage, Scene scene) {
+			this.scene = scene;
+			this.stage = stage;
+			this.handleGestures();
+		}
+		private boolean contextDrag(MouseEvent e){
+			boolean state = e.getSceneX()>20 && e.getSceneY()>20 && e.getSceneX()<scene.getWidth()-20 && e.getSceneY()<scene.getHeight()-20;
+			return state;
+		}
+		private void handleGestures() {
+
+			baseWidth = GameSettings.WIDTH-100;
+			baseHeight = GameSettings.HEIGHT - 50;
+
+			scene.setOnScroll(e -> {
+
+				if (e.isControlDown()) {
+
+					double factor = e.getDeltaY();
+				    double oldScale = stage.getHeight()/stage.getWidth();
+				    double scale = oldScale * e.getDeltaY();
+
+					double aspectRatioX = factor > 1 ? 1.01 : 0.99;
+					double aspectRatioY = factor > 1 ? 1.01 : 0.99;
+
+					if (!stage.isFullScreen()) {
+
+						zoom(stage, e);
+//
+//					    if (scale < 0.15){ scale = 0.15;}
+//					    if (scale > 1){scale = 1.015;}
+//
+//						baseWidth = baseWidth * scale;
+//						baseHeight = baseHeight * scale;
+//
+//						stage.setWidth(baseWidth);
+//						stage.setHeight(baseHeight);
+
+						if(baseWidth>GameSettings.SCREEN_WIDTH+10 && baseHeight>GameSettings.SCREEN_HEIGHT){
+							stage.setWidth(GameSettings.WIDTH);
+							stage.setHeight(GameSettings.HEIGHT);
+							baseWidth = GameSettings.WIDTH-100;
+							baseHeight = GameSettings.HEIGHT - 50;
+							stage.setFullScreen(true);
+						}
+					}
+				}
+			});
+		}
+		@Override
+		public void handle(MouseEvent mouseEvent) {
+			EventType<? extends MouseEvent> mouseEventType = mouseEvent.getEventType();
+			Scene scene = stage.getScene();
+
+			double mouseEventX = mouseEvent.getSceneX();
+			double mouseEventY = mouseEvent.getSceneY();
+			double sceneWidth = scene.getWidth();
+			double sceneHeight = scene.getHeight();
+
+
+			if (!stage.isFullScreen()) {
+
+				if (MouseEvent.MOUSE_MOVED.equals(mouseEventType) == true) {
+					if (mouseEventX > sceneWidth - border*6 && mouseEventY > sceneHeight - border*6) {
+						cursorEvent = GameImageBank.diagonalDragNW_SE;
+					} else if (mouseEventX > sceneWidth - border*6) {
+						cursorEvent = GameImageBank.horizontalDrag;
+					} else if (mouseEventY > sceneHeight - border*6) {
+						cursorEvent = GameImageBank.verticalDrag;
+					} else {
+						cursorEvent = GameImageBank.normalCursor;
+					}
+						scene.setCursor(cursorEvent);
+
+				}
+				else if (MouseEvent.MOUSE_EXITED.equals(mouseEventType) || MouseEvent.MOUSE_EXITED_TARGET.equals(mouseEventType)) {
+					CursorUtility.setCursor(CursorID.NORMAL, scene);
+				}
+				else if (MouseEvent.MOUSE_PRESSED.equals(mouseEventType) == true) {
+
+					if(!mouseEvent.isControlDown()){
+						xOffset = stage.getX() - mouseEvent.getScreenX();
+						yOffset = stage.getY() - mouseEvent.getScreenY();
+
+						stretchContextX = mouseEvent.getSceneX();
+						stretchContextY = mouseEvent.getSceneY();
+					}
+				}
+				else if (MouseEvent.MOUSE_CLICKED.equals(mouseEventType)){
+					stretchContextX = mouseEvent.getSceneX();
+					stretchContextY = mouseEvent.getSceneY();
+					if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+	                    if (mouseEvent.getClickCount() == 2) {
+	                    	if(!stage.isFullScreen()){
+	                        stage.setFullScreen(true);
+	                    	}
+	                    	else if(stage.isFullScreen()){
+	                    	stage.setFullScreen(false);
+	                    	}
+	                    }
+	                }
+				}
+				else if (MouseEvent.MOUSE_DRAGGED.equals(mouseEventType) == true) {
+
+
+						if (!stage.isFullScreen() && !mouseEvent.isControlDown()) {
+							if(contextDrag(mouseEvent)){
+								CursorUtility.setCursor(CursorID.DRAG, scene);
+								stage.setX(mouseEvent.getScreenX() + xOffset);
+								stage.setY(mouseEvent.getScreenY() + yOffset);
+							}
+							else{
+
+								if(mouseEvent.getSceneX()>stretchContextX){
+									factor = 1.001;
+								}
+								else if(mouseEvent.getSceneX()<stretchContextX){
+									factor = 0.999;
+								}
+								if(mouseEvent.getSceneY()>stretchContextY){
+									factor = 1.001;
+								}
+								else if(mouseEvent.getSceneY()<stretchContextY){
+									factor = 0.999;
+								}
+
+								double aspectRatioX = factor > 1 ? 1.05 : 0.995;
+								double aspectRatioY = factor > 1 ? 1.05 : 0.995;
+
+								baseWidth = baseWidth * aspectRatioX;
+								baseHeight = baseHeight * aspectRatioY;
+
+								stage.setWidth(baseWidth);
+								stage.setHeight(baseHeight);
+
+								if(baseWidth>GameSettings.SCREEN_WIDTH+50 && baseHeight>GameSettings.SCREEN_HEIGHT){
+									stage.setWidth(GameSettings.WIDTH);
+									stage.setHeight(GameSettings.HEIGHT);
+									baseWidth = GameSettings.WIDTH-100;
+									baseHeight = GameSettings.HEIGHT - 50;
+									stage.setFullScreen(true);
+								}
+							}
+
+					}
+				}
+			}
+		}
+		public void zoom(Stage stage, double scaleFactor, double x, double y) {
+
+		    double oldScale = 1;
+		    double scale = oldScale * scaleFactor;
+
+		    if (scale < 0.15){ scale = 0.15;}
+		    if (scale > 1){scale = 1.015;}
+
+		    baseWidth = baseWidth * scale;
+			baseHeight = baseHeight * scale;
+
+		    stage.setWidth(baseWidth);
+		    stage.setHeight(baseHeight);
+
+		}
+
+		public void zoom(Stage stage, ScrollEvent event) {
+		    zoom(stage, Math.pow(1.001, event.getDeltaY()), event.getSceneX(), event.getSceneY());
+		}
+	}
+
+
+}
---
+++ Personal JavaFX Snake/src/com/EudyContreras/Snake/HUDElements/GameBorder.java
@@ -0,0 +1,73 @@
+package com.EudyContreras.Snake.HUDElements;
+
+import com.EudyContreras.Snake.AbstractModels.AbstractTile;
+import com.EudyContreras.Snake.FrameWork.GameManager;
+import com.EudyContreras.Snake.FrameWork.GameSettings;
+import com.EudyContreras.Snake.ImageBanks.GameImageBank;
+
+import javafx.scene.Group;
+import javafx.scene.image.ImageView;
+
+/**
+ * Bounds class which can be used for adding specific events to objects
+ * that surround the level
+ *
+ * @author Eudy Contreras
+ *
+ */
+public class GameBorder extends AbstractTile {
+	private double borderSize = 10;
+	private ImageView leftBound;
+	private ImageView rightBound;
+	private ImageView topBound;
+	private ImageView bottomBound;
+	private Group layer;
+
+	public GameBorder(GameManager game, Group layer) {
+		this.velX = 0;
+		this.view.setTranslateX(x);
+		this.view.setTranslateY(y);
+		this.layer = layer;
+		setUpBounds();
+	}
+	public void setUpBounds(){
+		this.leftBound = new ImageView(GameImageBank.vertical_border);
+		this.rightBound = new ImageView(GameImageBank.vertical_border);
+		this.topBound = new ImageView(GameImageBank.horizontal_border);
+		this.bottomBound = new ImageView(GameImageBank.horizontal_border);
+
+		this.setDimensions_h(bottomBound);
+		this.setDimensions_h(topBound);
+		this.setDimensions_v(leftBound);
+		this.setDimensions_v(rightBound);
+
+		this.leftBound.setX(0);
+		this.rightBound.setX(GameSettings.WIDTH - borderSize);
+		this.topBound.setX(0);
+		this.topBound.setY(0);
+		this.bottomBound.setX(0);
+		this.bottomBound.setY(GameSettings.HEIGHT - borderSize);
+		this.displayBounds();
+	}
+	public void setDimensions_h(ImageView view){
+		view.setFitWidth(GameSettings.WIDTH);
+		view.setFitHeight(borderSize);
+	}
+	public void setDimensions_v(ImageView view){
+		view.setFitWidth(borderSize);
+		view.setFitHeight(GameSettings.HEIGHT);
+	}
+	public void displayBounds(){
+		this.layer.getChildren().add(leftBound);
+		this.layer.getChildren().add(rightBound);
+		this.layer.getChildren().add(topBound);
+		this.layer.getChildren().add(bottomBound);
+	}
+	public void showBounds(boolean state){
+		this.leftBound.setVisible(state);
+		this.rightBound.setVisible(state);
+		this.topBound.setVisible(state);
+		this.bottomBound.setVisible(state);
+	}
+
+}
